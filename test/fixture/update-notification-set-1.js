f1ce1b74622c17d28026e31784acbb2e76877b6c
ndelvalle <nicolas.delvalle@gmail.com>
2016-12-05 00:17:49 -0300

    Refactor using notificationSet model

---
+++ test/fixture/update-notification-set-1.js
@@ -0,0 +1,10 @@
+// const ObjectId = require('mongodb').ObjectId;
+
+
+const updateNotificationSet1 = {
+  _id  : '12a3d077c143c921072e342a',
+  title: 'Notification Foo updated'
+};
+
+
+module.exports = updateNotificationSet1;
